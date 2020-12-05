52d73d1484373121ad3999420841890c5845e5e7
teja156 <tejaswaroop156@gmail.com>
2020-12-05 10:46:10 +0530

    mod models

--- MyServer/models.py
+++ MyServer/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Links(models.Model):
-	short_url = models.CharField(max_length = 20)
+	short_url = models.CharField(max_length = 100)
 	redirect_url = models.TextField()
-	tracking_url = models.CharField(max_length = 20)
+	tracking_url = models.CharField(max_length = 100)
 
 
 class TrackingData(models.Model):
-	tracking_url = models.CharField(max_length=20)
-	short_url = models.CharField(max_length=20)
+	tracking_url = models.CharField(max_length=100)
+	short_url = models.CharField(max_length=100)
 	ip_address = models.TextField()
 	browser = models.TextField()
 	os = models.TextField()
